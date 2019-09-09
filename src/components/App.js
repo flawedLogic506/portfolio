4a6ffe888df9351d25229856dac7f88096987c5e
Jorge A <ardonz@gmail.com>
2019-09-09 16:55:02 -0600

    initial deployment

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+/** @jsx jsx */
+import {css, jsx} from '@emotion/core';
+
+import Sidebar from './Sidebar';
+import Content from './Content';
+
+const App = () => (
+  <div css={css`
+    display: flex;
+    width: 100%;
+    padding: 0;
+  `}
+  
+  >
+    <Sidebar />
+    <Content />
+  </div>
+);
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import {css, jsx} from '@emotion/core';
+
+const Sidebar = () => (
+  <div css={css`
+    width: 20%;
+    background: salmon;
+    min-height: 100vh;
+  `}>
+    Sidebar
+  </div>
+);
+
+export default Sidebar;
